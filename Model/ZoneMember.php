b53b9c2aa845eb48e88974a24ad959d4b7ec38a6
Rajesh Sharma <broncha@rajesharma.com>
2018-05-14 15:28:56 +0545

    Update ZoneMember.php

--- Model/ZoneMember.php
+++ Model/ZoneMember.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace Sylius\Component\Addressing\Model;
 
-class ZoneMember implements ZoneMemberInterface
+abstract class ZoneMember implements ZoneMemberInterface
 {
     /**
      * @var mixed
